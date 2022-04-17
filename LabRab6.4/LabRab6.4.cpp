b651106b7a30938306d2583ef6af940c6ca4201b
GEREGUR <georgerubaylo@gmail.com>
2022-04-17 18:18:24 +0300

    LabRab6.4

---
+++ LabRab6.4/LabRab6.4.cpp
@@ -0,0 +1,32 @@
+﻿#include <set>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	set <int> akk; 
+	akk.insert(0); 
+	while (true)
+	{
+		int id;
+		cout << "Введите ID пользователя\n";
+		cin >> id;
+		if (akk.find(id) == akk.end()) 
+		{
+			akk.insert(id);
+		}
+		else
+		{
+			cout << "Повторное использование аккаунта с ID " << id << endl;
+			break;
+		}
+	}
+
+
+
+
+
+
+}
