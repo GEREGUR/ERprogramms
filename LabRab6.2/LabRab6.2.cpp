226da93fcb6e7894e20765121c05d3960d2d80bc
GEREGUR <georgerubaylo@gmail.com>
2022-04-17 18:12:38 +0300

    LabRab6.2

---
+++ LabRab6.2/LabRab6.2.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <deque>
+
+using namespace std;
+
+int main()
+{
+
+    setlocale(LC_ALL, "rus");
+    deque <char> deq;
+    int g;
+    cout << "Введите количество элементов: ";
+    cin >> g;
+    char l;
+    cout << "Введите элементы по одному\n";
+    for (int i = 0; i < g; i++)
+    {
+        cin >> l;
+        deq.push_back(l);
+    }
+  
+    auto start = deq.begin();
+    auto finish = deq.end() - 1;
+    bool flag = true;
+    while (start != finish)
+    {
+        if ((*start) != (*finish))
+        {
+            flag = false;
+            break;
+        }
+        else
+        {
+            deq.pop_back();
+            deq.pop_front();
+        }
+        start = deq.begin();
+        finish = deq.end() - 1;
+    }
+    if (flag == false)
+    {
+        cout << "No" << endl;
+    }
+    else
+    {
+        cout << "Yes" << endl;
+    }
+
+}
+
+
