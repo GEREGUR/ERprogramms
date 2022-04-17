37ae99ac60297950efa2664901869360baa46bff
GEREGUR <georgerubaylo@gmail.com>
2022-04-17 18:09:40 +0300

    LabRab6.1

---
+++ LabRab6.1/LabRab6.1.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    setlocale(0, "rus");
+    bool flag = true;
+    char str[] = "2*(3-4)+[{3-1} + (4-2)]";
+    stack <char> st;
+    for (int i = 0; i < strlen(str); i++)
+    {
+        if (str[i] == '(' || str[i] == '[' || str[i] == '{')
+        {
+            st.push(str[i]);
+        }
+        else
+        {
+            if (str[i] == ')' || str[i] == ']' || str[i] == '}')
+            {
+                if ((str[i] == ')' && st.top() == '(') || (str[i] == ']' && st.top() == '[') || (str[i] == '}' && st.top() == '{'))
+                {
+                    st.pop();
+                }
+                else
+                {
+                    flag = false;
+                    break;
+                }
+            }
+        }
+    }
+    if (flag == true)
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+
+}
