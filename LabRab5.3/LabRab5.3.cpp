5d31df91f72369313350637855f8fcada472dd95
GEREGUR <georgerubaylo@gmail.com>
2022-04-18 08:51:48 +0300

    LabRab5.3

---
+++ LabRab5.3/LabRab5.3.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include "Matrix3DG.h"
+
+using namespace std;
+
+int main()
+{
+    setlocale(0, "rus");
+    double tr;
+    int z;
+    cout << "Введите размер матрицы: ";
+    cin >> z;
+    cout << "Работает конструктор\n";
+    Matrix3DG matr1 = Matrix3DG(z);
+    cout << "";
+  
+    cout << "Введите Трехдиагональную матрицу\n";
+    matr1.input();
+  
+    matr1.print();
+    tr = matr1.trace();
+    cout << "След = " << tr << endl;
+    cout << "Определитель матриц " << matr1.det() << endl;
+    cout << matr1.get_n(); 
+    cout << matr1 << endl;
+    Matrix3DG matr2 = Matrix3DG(8);
+    matr2.inputRand();
+    cin >> matr2;
+    cout << matr1 << matr2 << endl;
+
+}
+
