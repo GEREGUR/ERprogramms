a2a35967a50f52ef7c66b6de0350ec40ac108ab7
GEREGUR <georgerubaylo@gmail.com>
2022-04-18 09:00:34 +0300

    Pair_7

---
+++ Pair_7/Pair_7.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include "Pair.h"
+using namespace std;
+
+Pair::Pair()
+{
+	this->age = 0;
+	this->family = "";
+}
+Pair::Pair(string family, int age)
+{
+	this->age = age;
+	this->family = family;
+}
+void Pair::show()
+{
+	cout << "Фамилия: " << family << "   " << "Возраст: " << age << endl;
+}
+bool operator<(const Pair& p1, const Pair& p2)
+{
+	return p1.age < p2.age;
+}
+bool operator>(const Pair& p1, const Pair& p2)
+{
+	return p1.age > p2.age;
+}
+bool operator<=(const Pair& p1, const Pair& p2)
+{
+	return p1.age <= p2.age;
+}
+bool operator>=(const Pair& p1, const Pair& p2)
+{
+	return p1.age >= p2.age;
+}
